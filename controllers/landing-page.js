80c376bab00a234984ad2b20fae9103ec086d85c
jt1402 <jturgunov1402@gmail.com>
2024-11-04 21:59:14 +0900

    updated

--- controllers/landing-page.js
+++ controllers/landing-page.js
@@ -1,6 +1,20 @@
 exports.getLandPage = (req, res, next) => {
+  res.render("main_screen", {
+    pageTitle: "project",
+    path: "/landPage",
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (username === "yourUsername" && password === "yourPassword") {
+    res.redirect("/dashboard"); 
+  } else {
     res.render("main_screen", {
-      pageTitle: "project",
-      path: "/landPage",
+      pageTitle: "Student Assistant",
+      path: "/",
+      errorMessage: "Invalid username or password", 
     });
-  };
+  }
+};
